0ca8ae2f0d0ef82edfbefda197d73bff2c0a5fb2
louisetbiz <louisetbiz@gmail.com>
2024-01-22 15:52:33 +0100

    Photos component created

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,15 +2,22 @@ import "./Dictionary.css"
 import Results from "./Results.js";
 import axios from "axios";
 import React,{ useState } from "react";
+import Photos from "./Photos.js"
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState(null);
     let [results, setResults] = useState (null);
+    let [loaded, setLoaded] = useState (false);
+    let [photos, setPhotos] =useState (null);
 
     function handleResponse(response){
-       
         setResults(response.data);
     }
+
+    function handlePhotoResponse(response){
+        setPhotos(response.data.photos);
+    }
+
     function search(event){
         event.preventDefault();
         alert(`Searching for ${keyword}`);
@@ -19,7 +26,13 @@ export default function Dictionary(){
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
 
-    }
+    let photoApiKey = "ee030ced13bec32faetaa24oa4e6af48";
+    let photoApiUrls =`https://api.shecodes.io/images/v1/search?query=${keyword}&key=${photoApiKey}
+    `;   
+    axios.get(photoApiUrls).then(handlePhotoResponse);
+
+
+}
 
     function handleKeywordChange(event){
         setKeyword(event.target.value);
@@ -34,6 +47,7 @@ export default function Dictionary(){
             </form>
             </section>
             <Results results={results} />
+            <Photos photos={photos} />
         </div>
         
     )
